4cd0acc570391da7c9ac78702686611f80dd7de6
ynadyana <hndayana7@gmail.com>
2026-01-18 19:01:48 +0800

    Fix authentication

--- backend/src/main/java/io/github/ynadyana/inventory_backend/auth/dto/AuthResponse.java
+++ backend/src/main/java/io/github/ynadyana/inventory_backend/auth/dto/AuthResponse.java
@@ -1,7 +1,17 @@
 package io.github.ynadyana.inventory_backend.auth.dto;
 
-public record AuthResponse(
-        String token,
-        String email,
-        String role
-) {}
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String role;
+    private String username;
+}
